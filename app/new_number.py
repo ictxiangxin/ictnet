95c9173cb70ff92537eff7d9a1aada28db0098ed
ictxiangxin <ictxiangxin@gmail.com>
2014-12-20 15:40:00 +0800

    add comment to app files

--- app/new_number.py
+++ app/new_number.py
@@ -3,6 +3,11 @@ __author__ = 'ict'
 from graph import Graph
 from utils import random_number
 
+# read original data
 g = Graph("../data/friend.txt")
+
+# load data
 g.read()
+
+# create random node number file
 random_number(g, "../data/friend-new.txt")
